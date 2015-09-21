5782dfc15b85fe7a82fa07a59086eb1d1b2e38bc
Matthias Wolf <matthias.wolf@cern.ch>
2015-09-21 11:08:07 +0200

    2015B is actually accessible.

--- test/files.sh
+++ test/files.sh
@@ -1,3 +1,3 @@
 rm Debug/HcalMetStudy/python/files_*.py
-wget -O Debug/HcalMetStudy/python/files_raw.py "https://cmsweb.cern.ch/das/makepy?dataset=/MET/Run2015C-v1/RAW&instance=prod/global"
-wget -O Debug/HcalMetStudy/python/files_reco.py "https://cmsweb.cern.ch/das/makepy?dataset=/MET/Run2015C-PromptReco-v1/RECO&instance=prod/global"
+wget -O Debug/HcalMetStudy/python/files_raw.py "https://cmsweb.cern.ch/das/makepy?dataset=/MET/Run2015B-v1/RAW&instance=prod/global"
+wget -O Debug/HcalMetStudy/python/files_reco.py "https://cmsweb.cern.ch/das/makepy?dataset=/MET/Run2015B-PromptReco-v1/RECO&instance=prod/global"
